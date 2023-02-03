e737b36a70283c8584175debbed770a3fb859adc
Tarun <tjocz26@gmail.com>
2023-02-03 22:21:17 +0530

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import codesync from "../assets/portfolio/codesync.png";
 import resumeBuilder from "../assets/portfolio/resumeBuilder.png";
-import installNode from "../assets/portfolio/installNode.jpg";
+import NetscapeToJSONConverter from "../assets/portfolio/NetscapeToJSONConverter.png";
+import TodoBot from "../assets/portfolio/TODOBotJava.png";
 import { FaGithub } from "react-icons/fa";
 import { TbExternalLink } from "react-icons/tb";
 const Portfolio = () => {
@@ -11,18 +12,28 @@ const Portfolio = () => {
       name: "CodeSync",
       src: codesync,
       href: "https://github.com/MrHacker26/codesync",
+      hrefLive: "https://codesync-tarun.vercel.app/",
     },
     {
       id: 2,
       name: "ResumeBuilder",
       src: resumeBuilder,
       href: "https://github.com/MrHacker26/ResumeBuilder",
+      hrefLive: "https://resume-builder-tarun.vercel.app/",
     },
     {
       id: 3,
-      src: installNode,
-      name: "NodeJS",
-      href: "https://github.com/MrHacker26/",
+      src: NetscapeToJSONConverter,
+      name: "NetscapeToJSONConverter",
+      href: "https://github.com/MrHacker26/netscapetojson",
+      hrefLive: "https://github.com/MrHacker26/netscapetojson",
+    },
+    {
+      id: 4,
+      src: TodoBot,
+      name: "Telegram TODO Bot",
+      href: "https://github.com/MrHacker26/todo-bot-telegram-java",
+      hrefLive: "https://github.com/MrHacker26/todo-bot-telegram-java",
     },
   ];
 
@@ -40,17 +51,19 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-10 sm:px-0">
-          {portfolios.map(({ id, name, src, href }) => (
+          {portfolios.map(({ id, name, src, href, hrefLive }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
                 className=" h-44 rounded-md duration-200 hover:scale-105"
               />
-              <p className=" text-lg font-bold text-gray-300 italic pl-4 pt-3 ">{name}</p>
+              <p className=" text-lg font-bold text-gray-300 italic pl-4 pt-3 ">
+                {name}
+              </p>
               <div className="flex items-center justify-center">
                 <a
-                  href={href}
+                  href={hrefLive}
                   target="_blank"
                   rel="noreferrer"
                   className="w-1/2 px-6 py-2 m-4 duration-200 hover:scale-105 flex border-2 border-gray-400 rounded-xl hover:bg-gray-700"

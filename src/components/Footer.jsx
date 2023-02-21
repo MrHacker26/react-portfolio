57df811731491d98f0377018dd3a85a4d2bd30ad
MrHacker26 <tjocz26@gmail.com>
2023-02-21 16:42:10 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer class="p-4 pl-16 shadow md:flex md:items-center md:justify-between md:p-6 bg-black h-32">
+    <footer class=" pb-20 justify-center items-center grid md:items-center md:justify-between md:p-6 bg-black h-32">
     <span class="text-sm sm:text-center text-gray-400">© 2023 <a href="/" class="hover:underline">Tarun</a>. All Rights Reserved.
     </span>
 </footer>
